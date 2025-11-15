f11baeadc1444ad011eba13f41fc8d1c50623191
abigrace8576 <abi.guzman@ossm.edu>
2025-11-14 20:40:45 -0600

    YEAH BUDDY
    Added implementations for Perfect, Cuckoo, and Hopscotch hashing.

--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+// this should print all the tables i need... gulp
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Integer> input = new ArrayList<>();
+        Scanner scanner = new Scanner(new File("TestSequence.txt"));
+        while (scanner.hasNextInt()) {
+            input.add(scanner.nextInt());
+        }
+//will this work???? yeahhhhhh
+        System.out.println("=== Perfect Hashing ===");
+        PerfectHashing perfect = new PerfectHashing(input);
+        perfect.printTable(); //yeah ur so perfect
+        System.out.println("\n=== Cuckoo Hashing ===");
+        CuckooHashing cuckoo = new CuckooHashing(13);
+        for (int key : input) {
+            cuckoo.insert(key);
+        }
+        cuckoo.printTable();
 
+        System.out.println("\n=== Hopscotch Hashing ===");
+        HopscotchHashing hopscotch = new HopscotchHashing(16, 4);
+        for (int key : input) {
+            hopscotch.insert(key);
+        }
+        hopscotch.printTable();
+    }
+}
